ca5fb12d396f146cccda784de5bc4e931f5bfeab
Riya505 <riyapk505@gmail.com>
2022-10-11 17:48:24 +0530

    navbar

---
+++ src/app/post-app/post-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-app',
+  templateUrl: './post-app.component.html',
+  styleUrls: ['./post-app.component.css']
+})
+export class PostAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
